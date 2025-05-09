e6602a909d591908f3360a65e4bfb9951052c3b9
FranciscoRomanS <frankar11294@gmail.com>
2025-05-09 11:53:48 +0200

    Prueba del dia de hoy

---
+++ jsP/main.js
@@ -0,0 +1,29 @@
+// Ejercicio 1
+// Declara una función que acepte una palabra como parámetro 
+// y la devuelva con todos sus caracteres en mayúsculas y separados por espacios. 
+// Ejemplo: Para "Ana" devolverá "A N A".
+
+// const ana = ["a", "n", "a"];     // Declaro el array con los elementos del nombre ana
+// ana.forEach(function (an) {console.log(an.toUpperCase())        // Mediante la funcion forEach recorro cada elemento 
+//                                                                // del array aplico el metodo toUpperCase 
+//                                                                // devolviendo cada elemento convertido en mayusculas 
+//                                                                // a la consola
+//  });
+// ______________________________________________________________
+const ana = ["a n a"];     
+ana.forEach(function (an) {console.log(an.toUpperCase())});
+
+
+// _________________________________________________________________________________________________________________
+
+// Ejercicio 2
+// Declara una función que acepte un nombre como parámetro y devuelva el número de palabras que tiene el nombre.
+//  Ejemplo: Para "Antonio Alberto Jesús" devolverá 3.
+
+
+// let nombre = ["Antonio", "Alberto", "Jesús"];
+// console.log(nombre.length);
+
+// ___________________________________________________________________________________________________________________
+// Ejercicio 3:
+// Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase.
